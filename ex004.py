aea09c9c281e658c2672fcdb1b082b7d8ad24ddb
Luis Pimenta <140265694+LuisPiment@users.noreply.github.com>
2023-07-25 17:34:56 +0100

    update

--- ex004.py
+++ ex004.py
@@ -9,12 +9,20 @@ class Janela:
         self.lb1=Label(self.fr1, text="Ola mundo")
         self.lb1.pack()
 
-        self.bt1=Button(self.fr1,text="CLIQUE AQUI")
+        self.bt1=Button(self.fr1,text="CLIQUE AQUI",command=self.criar_text)
+        self.bt1.bind("<Return>",self.criar_text2)
         self.bt1.pack()
         
         self.fr2=Frame(raiz2)
         self.fr2.pack()
-
+    
+    def criar_text(self):
+        self.lb2=Label(self.fr1,text="Opa")
+        self.lb2.pack()
+    
+    def criar_text2(self,event):
+        self.lb3=Label(self.fr1,text="Entendo")
+        self.lb3.pack()
 
 raizz=Tk()
 raiz=Tk()

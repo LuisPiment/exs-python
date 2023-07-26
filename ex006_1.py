7cdcc57116772537ac3880564b026adfacb09d8f
Luis Pimenta <140265694+LuisPiment@users.noreply.github.com>
2023-07-26 12:34:15 +0100

    att

---
+++ ex006_1.py
@@ -0,0 +1,20 @@
+"""Preparações para o projeto do ex006"""
+cinza1="#1C1C1C"
+cinza2="#A9A9A9"
+branco="#ffffff"
+preto="_#000000"
+azul_claro="#E0FFFF"
+azul2="#A0E0D0"
+vermelho="red"
+fonte1=("Time New Roman",30,"bold")
+fonte2=("Verdana",15,"bold")
+fonte2=("Verdana",20,"bold")
+fonte2=("Verdana",12,"bold")
+def par_impar(num1,num2):
+    s=num1+num2
+    if s%2==1:
+        pi=("Impar")
+    else:
+        pi=("Par")
+    return pi
+

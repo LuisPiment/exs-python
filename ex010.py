b83abbd306494a62329655047d68f158a3d25fce
Luis Pimenta <140265694+LuisPiment@users.noreply.github.com>
2023-08-24 23:03:07 +0100

    .

--- ex010.py
+++ ex010.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get("/")
+@app.get("/suada/{nome}")
 
-async def top():
-    return("Foi um bom começo 22/08/2023")
+def top(nome):
+    return(f"Tu {nome} es gay ")
+
+@app.get("/area") #http://127.0.0.1:8000/area?largura=9&comprimento=9
+
+def areatot(largura: int =2, comprimento: int =2):
+    return(f"A area é {largura*comprimento}")
 

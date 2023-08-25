aee7010d201deda75667d78a49e51c18f3c0713d
Luis Pimenta <140265694+LuisPiment@users.noreply.github.com>
2023-08-25 18:58:30 +0100

    .

---
+++ ex011.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from random import randint
+animal=[]
+lista=[]
+info=[]
+animais=FastAPI()
+class Cadas(BaseModel):
+    nome:str
+    idade:int
+    sexo:str
+    cor:str
+
+@animais.post("/animais")
+def resposta(cadastro:Cadas):
+    id=randint(1000,9999)
+    animal.append(id)
+    animal.append(cadastro.nome)
+    animal.append(cadastro.idade)
+    animal.append(cadastro.sexo)
+    animal.append(cadastro.cor)
+    lista.append(animal.copy())
+    animal.clear()
+    return(f"O animal {cadastro.nome}, com o {cadastro.idade} anos, do sexo {cadastro.sexo} e da cor {cadastro.cor} foi registrado com sucesso")
+
+@animais.get("/animais")
+def lista1():
+    return(lista)
+
+@animais.get("/animais/{id1}")
+def animal2(id1):
+    for c in lista:
+        for a in c:
+            print(a)
+            print("hmmmmmmmm")
+            print(id1)
+            if a == id1:
+                print("deu certo")
+                info.append(lista[c][:])
+                print(info)
+            else:
+                print("deu errado")
+    print(lista)

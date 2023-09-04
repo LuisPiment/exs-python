46342e44dbfa93665ae6e22ab022a29cdb9e120e
Luis Pimenta <140265694+LuisPiment@users.noreply.github.com>
2023-09-04 16:10:12 +0100

    .

---
+++ ex013.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+from typing import Optional,List
+from random import randint
+import sqlite3
+
+
+
+
+class Produto(BaseModel):
+    id: Optional [int]=randint(100,999) 
+    nome_Prod: str
+    vendedor:Usuario
+    detalhes: str
+    preço:float
+    disponivel: bool =False
+
+
+class Pedido(BaseModel):
+    id: Optional [int]=randint(100,999) 
+    produto:Produto
+    comprador:Usuario
+    quantidade: int
+    status_entrega:bool=False
+    destinho: str
+
+class Usuario(BaseModel):
+    id: Optional [int]=randint(100,999) 
+    nome: str 
+    idade: int
+    email: str
+    pedidos: List[Pedido]
+    produtos_a_venda: List[Produto]
+    minhas_vendas: List[Pedido]
+    telefone: str
+

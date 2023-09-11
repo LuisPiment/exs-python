10e3c45e669db6ee9f2336aa5fa5deee2adb5d6c
Luis Pimenta <140265694+LuisPiment@users.noreply.github.com>
2023-09-11 19:24:56 +0100

    l

--- ex013.py
+++ ex013.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query,Body
+from fastapi import FastAPI, Query,Body,HTTPException
 from pydantic import BaseModel
 from typing import Union,List,Set
 app=FastAPI()
@@ -11,11 +11,14 @@ class Toma(BaseModel):
     idade:int =None
     cidade: morada=None
 
-@app.get("/items")
-def root(largura:int =0, comprimento:int =Query(gt=50,lt=90) ):
-    area=largura*comprimento
-    return(f"A area é {area} e os nomes são ")
+@app.get("/items",tags=["items"])
+def root(largura:int =0, comprimento:int =0):
+    if largura>50 or comprimento>50:
+        raise HTTPException(status_code=404,detail="O comprimento e a largura tem de ser menores que 50")
+    else:
+        area=largura*comprimento
+        return(f"A area é {area} e os nomes são ")
 
-@app.post("/pessoa")
+@app.post("/pessoa",tags=["items"])
 def caminho(toma:Toma,lingundo:int =0):
     return(toma.cidade)
